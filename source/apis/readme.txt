09a03e0079ce0808f46d0da110da5d4da95512e2
OlegSuperBro <olegrakovic323@gmail.com>
2022-07-05 12:27:56 +0300

    first commit

---
+++ source/apis/readme.txt
@@ -0,0 +1,5 @@
+API file HAVE to contain
+
+"api" class     - main class with all functions
+"start" func    - function thst run on program start
+"get_data" func - function to get gata should give dict with data
---
+++ source/settings.py
@@ -0,0 +1,17 @@
+discord_token = ""
+
+game      = "osu" # game name
+wait_time = 10    # don't recommend less then 10 sec, because api ban
+
+# save. Based on api
+save_data = {
+    
+}
+
+
+# everything in "{}" executes like python code to ignore use "\{" or "\}"
+ 
+lines = [
+    "{statistics.pp}pp",
+    "{statistics.hit_accuracy}%"
+]
